f1b82587770f21fc5ed509e0f89a79aa505bd638
wendellxavier <wendell_xw20@hotmail.com>
2024-07-18 19:23:45 -0300

    " css "

--- projeto/settings.py
+++ projeto/settings.py
@@ -101,8 +101,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
