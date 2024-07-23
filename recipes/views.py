911b7cffac7d213223d351afac935bd49e0763f9
wendellxavier <wendell_xw20@hotmail.com>
2024-07-23 09:33:28 -0300

    "return 404"

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -7,8 +8,15 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request,'recipes/pages/category.html', {'recipes': recipes})
+    
+    if not recipes:
+        raise Http404('Not found')
+    
+    return render(request,'recipes/pages/category.html', {'recipes': recipes, 'title': f'{recipes.first().category.name}'})
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(id=id, is_published=True).order_by('-id').first()
+    
+    
     return render(request,'recipes/pages/recipe-view.html', {'is_detail_page': True, 'recipe': recipe,})
