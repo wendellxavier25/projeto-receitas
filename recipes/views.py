acb43b75c3fcec552d6e7fd2dca89c5612004fef
wendellxavier25 <wendellx25@gmail.com>
2024-07-31 11:30:54 -0300

    "Q"

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Recipe
 from django.http import Http404
+from django.db.models import Q
 
 def home(request):
     recipes = get_list_or_404(Recipe.objects.filter(is_published=True,).order_by('-id'))
@@ -26,6 +27,6 @@ def search(request):
     if not search_term:
         raise Http404()
     
-    recipes = Recipe.objects.filter(title__icontains=search_term)
+    recipes = Recipe.objects.filter(Q(title__icontains=search_term) | Q(description__icontains=search_term)).order_by('-id')
     
     return render (request, 'recipes/pages/search.html', {'page_title': f'Search for "{search_term}"', 'search_term': search_term, 'recipes': recipes})
