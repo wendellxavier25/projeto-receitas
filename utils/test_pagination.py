e72c2986c7e5356d464985e685226b2533070428
wendellxavier25 <wendellx25@gmail.com>
2024-08-03 11:14:50 -0300

    "teste"

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -9,3 +9,4 @@ class Pagination(TestCase):
         self.assertEqual([1, 2, 3, 4], pagination)
         
     def  test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
+        pass
