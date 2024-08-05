a95441297573191dd08d7581eb03e6c130f7d59b
wendellxavier25 <wendellx25@gmail.com>
2024-08-05 08:15:34 -0300

    "start is stop page"

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,2 +1,6 @@
-def make_pagination_range(page_range, qty_paginas, current_page):
-    return [1, 2, 3, 4]
+import math
+def make_pagination_range(page_range, qty_pages, current_page):
+    middle_range = math.ceil(qty_pages / 2) 
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+    return page_range[start_range:stop_range]
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -5,8 +5,18 @@ from utils.pagination import make_pagination_range
 class Pagination(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(page_range=list(range(1, 21)),
-                                           qty_paginas=4, current_page=1)
+                                           qty_pages=4, current_page=1)
         self.assertEqual([1, 2, 3, 4], pagination)
         
     def  test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        pass
+        pagination = make_pagination_range(page_range=list(range(1, 21)),
+                                           qty_pages=4, current_page=1)
+        self.assertEqual([1, 2, 3, 4], pagination)
+        
+        pagination = make_pagination_range(page_range=list(range(1, 21)),
+                                           qty_pages=4, current_page=2)
+        self.assertEqual([1, 2, 3, 4], pagination)
+        
+        pagination = make_pagination_range(page_range=list(range(1, 21)),
+                                           qty_pages=4, current_page=3)
+        self.assertEqual([2, 3, 4, 5], pagination)
